6cc9266f537dfbb7d8453e822e11c7984a1d80b6
Jose Lopes <946393361bill@gmail.com>
2021-07-12 17:48:26 +0100

    usando o useEffect para salvar os estados

--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,7 +1,8 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect } from 'react';
 import { TodoContextType } from './TodoContextType';
 import {Todo} from '../models/Todo'
-import { get } from '../services/TodoServices';
+import { get, save } from '../services/TodoServices';
+
 
 
 export const TodoContext = createContext<TodoContextType>({
@@ -15,22 +16,27 @@ const TodoProvider = (props: any) => {
 
     const [todos, setTodos] = useState<Todo[]>(get);
     
+    useEffect(() => {
+        save(todos);
+    }, [todos]);
 
     const addTodo = (title: string) => {
         const todo: Todo = {id: todos.length + 1, title: title, done: false}
-        setTodos([...todos, todo])
+        setTodos([...todos, todo]);
+     
     };
 
     const removeTodo = (todo: Todo) => {
         const index = todos.indexOf(todo);
         setTodos(todos.filter((_, i) => i !== index));
+        
     };
 
     const toggle = (todo: Todo) => {
         const index = todos.indexOf(todo);
         todos[index].done = !todo.done;
         setTodos([...todos])
-
+        
     }
 
     return(
