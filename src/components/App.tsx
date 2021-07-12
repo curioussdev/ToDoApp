bf7d0386b782e35dfa34bd898baa198bb683006c
Jose Lopes <946393361bill@gmail.com>
2021-07-12 21:20:49 +0100

    trabalhando com react form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,19 +2,34 @@ import React from 'react';
 import TodoContext from '../contexts/TodoContext';
 import Navbar from './Navbar';
 import TodoList from './TodoList';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 
 
 const App = () => {
     return(
         <TodoContext>
-            <div className="uk-container">
+            <Router>
                 <Navbar></Navbar>
-                <TodoList></TodoList>
-            </div>
-        </TodoContext>
-    );
-}
+                <br />
+                <div className="uk-container">
+                    <Switch>
+                        <Route path="/create">
+                          
+                        </Route>
 
+                        <Route path="/">
+                            <h4>Minha Lista de Tarefas</h4>
+                            <TodoList></TodoList>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </TodoContext>
+    
+    );
+
+};
 
 export default App;
