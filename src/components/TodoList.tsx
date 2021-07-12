4740cb64e2f7e11897e5d036fad1c2ef4c27f935
Jose Lopes <946393361bill@gmail.com>
2021-07-12 01:59:57 +0100

    tipando

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { Todo } from '../models/Todo'
 
 const TodoList = () => {
+    const todos: Todo[] = [
+        {id: 1, title: "Ir ao supermercado", done: false},
+        {id: 2, title: "Ir a academia", done: false}
+    ];
+    
+    
     return(
         <div>TodoList</div>
     );
