473415e8d8bd277ab38bf58413ed0a991a9c101a
Jose Lopes <946393361bill@gmail.com>
2021-07-12 02:22:10 +0100

    criando o item da lista

---
+++ src/components/TodoListItem.tsx
@@ -0,0 +1,10 @@
+import { directive } from '@babel/types';
+import React from 'react';
+
+const TodoLsitItem = () => {
+    return(
+        <div>Item</div>
+    );
+};
+
+export default TodoLsitItem;
