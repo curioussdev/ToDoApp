562088924a1ef622114dad59ad4a43b989a1fa19
Jose Lopes <946393361bill@gmail.com>
2021-07-12 16:27:29 +0100

    trabalhando com Providers

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -5,8 +5,7 @@ import TodoLsitItem from './TodoListItem';
 
 const TodoList = () => {
     const todos: Todo[] = [
-        {id: 1, title: 'Ir ao supermercado', done: false},
-        {id: 2, title: 'Ir a academia', done: false},
+       
      
     ];
     
--- src/contexts/TodoContext.tsx
+++ src/contexts/TodoContext.tsx
@@ -1,5 +1,6 @@
 import React, {createContext} from 'react';
 import { TodoContextType } from './TodoContextType';
+import {Todo} from '../models/Todo'
 
 
 export const TodoContext = createContext<TodoContextType>({
@@ -8,3 +9,32 @@ export const TodoContext = createContext<TodoContextType>({
     removeTodo: () => {},
     toggle: () => {}
 })
+
+const TodoProvider = (props: any) => {
+
+    const todos: Todo[] = [
+        
+        {id: 1, title: 'Ir ao supermercado', done: false},
+        {id: 2, title: 'Ir a academia', done: false},
+    ];
+
+    addTodo = (title: string) => {
+        console.log('Adicionou ' + title)
+    };
+
+    removeTodo = (todo: Todo) => {
+        console.log('Removeu' + todo.title)
+    };
+
+    toggle = (todo: Todo) => {
+        console.log('Alterou' + todo.title)
+    }
+
+    return(
+        <TodoContext.Provider value={{todos, addTodo, removeTodo, toggle}}>
+            {props.children}
+        </TodoContext.Provider>
+    );
+};
+
+export default TodoProvider;
